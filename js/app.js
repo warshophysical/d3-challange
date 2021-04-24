7a9a4a18b951c52415719009b9a8a0ec88809d62
warshophysical <73259613+warshophysical@users.noreply.github.com>
2021-04-24 13:11:21 -0500

    x-axis

--- js/app.js
+++ js/app.js
@@ -9,3 +9,40 @@ var svg = d3
   .attr("width", width)
   .attr("height", height)
   .attr("class", "chart");
+
+
+
+ // Margin for graph
+ var margin = 20;
+
+ // space for placing words
+ var labelArea = 110;
+
+ // padding for the text
+ var tPadBot = 40;
+ var tPadLeft = 40;
+
+ // Creating bottom axis
+
+ svg.append("g").attr("class", "xText");
+ var xText = d3.select(".xText");
+
+ function xTextRefresh() {
+   xText.attr(
+     "transform",
+     "translate(" +
+       ((width - labelArea) / 2 + labelArea) +
+       ", " +
+       (height - margin - tPadBot) +
+       ")"
+   );
+ }
+ xTextRefresh();
+
+ xText
+   .append("text")
+   .attr("y", -26)
+   .attr("data-name", "poverty")
+   .attr("data-axis", "x")
+   .attr("class", "aText active x")
+   .text("In Poverty (%)");
