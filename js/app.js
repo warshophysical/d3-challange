95badead65c36c569049705894354f18199531ac
warshophysical <73259613+warshophysical@users.noreply.github.com>
2021-04-24 23:14:34 -0500

    Dynamic Graph

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
+// Creating canvas for graph
 
-// creating canvas for graph
+// Width and Height of Containing Box
 var width = parseInt(d3.select("#scatter").style("width"));
 var height = width - width / 3.9;
+
 
 var svg = d3
   .select("#scatter")
@@ -17,9 +19,10 @@ var margin = 20;
 // space for placing words
 var labelArea = 110;
 
-// padding for the text
+// padding for the text of axis
 var tPadBot = 40;
 var tPadLeft = 40;
+
 
 
 // Creating bottom axis
@@ -27,6 +30,7 @@ var tPadLeft = 40;
 svg.append("g").attr("class", "xText");
 var xText = d3.select(".xText");
 
+/// Adding xText to chart
 function xTextRefresh() {
   xText.attr(
     "transform",
@@ -41,114 +45,221 @@ xTextRefresh();
 
 //Poverty
 xText
-.append("text")
-.attr("y", -26)
-.attr("data-name", "poverty")
-.attr("data-axis", "x")
-.attr("class", "aText active x")
-.text("In Poverty (%)");
-
-//Age
+  .append("text")
+  .attr("y", -26)
+  .attr("data-name", "poverty")
+  .attr("data-axis", "x")
+  .attr("class", "aText active x")
+  .text("In Poverty (%)");
+// Age
 xText
-.append("text")
-.attr("y", 0)
-.attr("data-name", "age")
-.attr("data-axis", "x")
-.attr("class", "aText inactive x")
-.text("Age (Median)");
-
-//Income
+  .append("text")
+  .attr("y", 0)
+  .attr("data-name", "age")
+  .attr("data-axis", "x")
+  .attr("class", "aText inactive x")
+  .text("Age (Median)");
+// Income
 xText
-.append("text")
-.attr("y", 26)
-.attr("data-name", "income")
-.attr("data-axis", "x")
-.attr("class", "aText inactive x")
-.text("Household Income (Median)");
+  .append("text")
+  .attr("y", 26)
+  .attr("data-name", "income")
+  .attr("data-axis", "x")
+  .attr("class", "aText inactive x")
+  .text("Household Income (Median)");
+
+// Creating left axis
+
+var leftTextX = margin + tPadLeft;
+var leftTextY = (height + labelArea) / 2 - labelArea;
 
 
- // Creating Left Axis
+svg.append("g").attr("class", "yText");
 
- var leftTextX = margin + tPadLeft;
- var leftTextY = (height + labelArea) / 2 - labelArea;
+var yText = d3.select(".yText");
 
+/// Adding yText to chart
+function yTextRefresh() {
+  yText.attr(
+    "transform",
+    "translate(" + leftTextX + ", " + leftTextY + ")rotate(-90)"
+  );
+}
+yTextRefresh();
 
-   svg.append("g").attr("class", "yText");
-   var yText = d3.select(".yText");
-
-   function yTextRefresh() {
-     yText.attr(
-       "transform",
-       "translate(" + leftTextX + ", " + leftTextY + ")rotate(-90)"
-     );
-   }
-
-   yTextRefresh();
-
-
-//Obesity
-  yText
+// Obesity
+yText
   .append("text")
   .attr("y", -26)
   .attr("data-name", "obesity")
   .attr("data-axis", "y")
   .attr("class", "aText active y")
-  .text("Obese (%)"); 
+  .text("Obese (%)");
 
 // Smokes
- yText
- .append("text")
- .attr("x", 0)
- .attr("data-name", "smokes")
- .attr("data-axis", "y")
- .attr("class", "aText inactive y")
- .text("Smokes (%)");
+yText
+  .append("text")
+  .attr("x", 0)
+  .attr("data-name", "smokes")
+  .attr("data-axis", "y")
+  .attr("class", "aText inactive y")
+  .text("Smokes (%)");
 
 // Lacks Healthcare
 yText
-.append("text")
-.attr("y", 26)
-.attr("data-name", "healthcare")
-.attr("data-axis", "y")
-.attr("class", "aText inactive y")
-.text("Lacks Healthcare (%)"); 
+  .append("text")
+  .attr("y", 26)
+  .attr("data-name", "healthcare")
+  .attr("data-axis", "y")
+  .attr("class", "aText inactive y")
+  .text("Lacks Healthcare (%)");
+
 
 //  sending data to browser
 //  d3.csv("assets/data/data.csv").then(function(data) {
 //  console.log(data);
 //  });
 
-// Visualize the data
+
+
+// Import our CSV data with d3's .csv import method.
 d3.csv("assets/data/data.csv").then(function(data) {
+  // Visualize the data
   visualize(data);
-  });
+});
 
 
-// Adding x-axis line
 function visualize(theData) {
+  
+  // Generating default groups
+  var curX = "poverty";
+  var curY = "obesity";
+
+  var xMin;
+  var xMax;
+  var yMin;
+  var yMax;
+
+  // change the min and max for x
+  function xMinMax() {
+    
+    xMin = d3.min(theData, function(d) {
+      return parseFloat(d[curX]) * 0.90;
+    });
+
+    
+    xMax = d3.max(theData, function(d) {
+      return parseFloat(d[curX]) * 1.10;
+    });
+  }
+
+  // b. change the min and max for y
+  function yMinMax() {
+    // min is smallest value from selected column
+    yMin = d3.min(theData, function(d) {
+      return parseFloat(d[curY]) * 0.90;
+    });
+
+    // max is greatest value from selected column
+    yMax = d3.max(theData, function(d) {
+      return parseFloat(d[curY]) * 1.10;
+    });
+  }
+
+  // activate switcher once clicked
+  function labelChange(axis, clickedText) {
+    d3
+      .selectAll(".aText")
+      .filter("." + axis)
+      .filter(".active")
+      .classed("active", false)
+      .classed("inactive", true);
+
+    clickedText.classed("inactive", false).classed("active", true);
+  }
+
+
+  // Grab the min and max values of x and y.
+  xMinMax();
+  yMinMax();
+
   
   var xScale = d3
     .scaleLinear()
+    .domain([xMin, xMax])
     .range([margin + labelArea, width - margin]);
- 
+
+  var yScale = d3
+    .scaleLinear()
+    .range([height - margin - labelArea, margin]);
+
   var xAxis = d3.axisBottom(xScale);
+  var yAxis = d3.axisLeft(yScale);
+
  
   svg
     .append("g")
-     .call(xAxis)
-     .attr("class", "xAxis")
-     .attr("transform", "translate(0," + (height - margin - labelArea) + ")");
+    .call(xAxis)
+    .attr("class", "xAxis")
+    .attr("transform", "translate(0," + (height - margin - labelArea) + ")");
+  svg
+    .append("g")
+    .call(yAxis)
+    .attr("class", "yAxis")
+    .attr("transform", "translate(" + (margin + labelArea) + ", 0)");
 
-     var yScale = d3
-     .scaleLinear()
-     .range([height - margin - labelArea, margin]);
-
-     var yAxis = d3.axisLeft(yScale);
-
-     svg
-     .append("g")
-     .call(yAxis)
-     .attr("class", "yAxis")
-     .attr("transform", "translate(" + (margin + labelArea) + ", 0)");
   
- }
+  // Dynamic Graph
+
+  d3.selectAll(".aText").on("click", function() {
+    
+    var self = d3.select(this);
+
+    if (self.classed("inactive")) {
+      var axis = self.attr("data-axis");
+      var name = self.attr("data-name");
+
+      if (axis === "x") {
+        curX = name;
+
+        xMinMax();
+
+        xScale.domain([xMin, xMax]);
+
+        // Update X Axis
+        svg.select(".xAxis").transition().duration(300).call(xAxis);
+
+      
+        d3.selectAll(".stateText").each(function() {
+          
+          d3
+            .select(this)
+            .transition()
+            .attr("dx", function(d) {
+              return xScale(d[curX]);
+            })
+            .duration(300);
+        });
+
+        labelChange(axis, self);
+      }
+      else {
+        
+        curY = name;
+
+        yMinMax();
+
+        yScale.domain([yMin, yMax]);
+
+        // Update Y Axis
+        svg.select(".yAxis").transition().duration(300).call(yAxis);
+
+      
+        
+        labelChange(axis, self);
+      }
+    }
+  });
+
+
+}
